42adededb46210264f05041c3d4d11139a2f16ec
01Anadoestouro <mora.ana@escola.pr.gov.br>
2026-05-26 14:16:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,44 @@ textos[j].classList.remove("ativo");for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
+contadores[0].textContent = calculaTempo(tempoObjetivo1);function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+}
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);return (
+  dias +
+  " dias " +
+  horas +
+  " horas " +
+  minutos +
+  " minutos " +
+  segundos +
+  " segundos"
+);function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}segundos %= 60;
+minutos %= 60;
+horas %= 24;.contador {
+  font-size: 20px;
+  text-align: center;
+
+  return horas + " horas";
+}
