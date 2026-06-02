4dd5b093cf4ba3d60cf3fcc8b14837f19d4c54c0
01Anadoestouro <mora.ana@escola.pr.gov.br>
2026-06-02 14:00:21 -0300

    Add HTML structure for countdown display

--- main.js
+++ main.js
@@ -37,6 +37,31 @@ let dias = Math.floor(horas / 24);return (
   let dias = Math.floor(horas / 24);
   return (
     dias +
+    //comecaCronometro();<div class="contador">
+  <div class="contador-digito"></div>
+</div><div class="contador">
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">dias</p>
+  </div>
+</div><div class="contador">
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">dias</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">horas</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">min</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">seg</p>
+  </div>
+</div>
     " dias " +
     horas +
     " horas " +
